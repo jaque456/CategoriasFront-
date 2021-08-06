e4471b9646889249e0bd69c03c773dea46867fb9
MarySTN <85576791+MarySTN@users.noreply.github.com>
2021-08-05 22:41:13 -0500

    Tabla para las categorías registradas
    Componente para la tabla donde se visualizan las categorías registradas

---
+++ src/app/verCategorias/verCategories.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-verCategories',
+    templateUrl: './verCategories.component.html'
+})
+export class verCategoriesComponent {
+
+}
